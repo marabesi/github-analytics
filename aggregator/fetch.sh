b3cdf14ed73aed2d9f15727754914422c8fbb99b
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-04-02 23:01:13 +0200

    chore(aggregator): renamed scripts

--- aggregator/fetch.sh
+++ aggregator/fetch.sh
@@ -2,5 +2,3 @@
 
 poetry run python fetch_workflow_runs.py
 poetry run python fetch_job_runs.py
-poetry run python fetch_coverage_running_time.py
-poetry run python plot.py
---
+++ aggregator/plot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+poetry run python fetch_coverage_running_time.py
+poetry run python plot.py
