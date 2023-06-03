8961f5ebbfd54a85eca71f663e6c2691a89805bc
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-06-03 21:53:34 +0200

    fix: url for build

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -3,19 +3,3 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import App from './App.vue'
-// import routes from './router.js'
-//
-// const router = new VueRouter({ routes })
-//
-// Vue.config.productionTip = false
-//
-// Vue.use(VueRouter)
-//
-// new Vue({
-//   router,
-//   render: h => h(App),
-// }).$mount('#app')
